8b26ba090557058ace13e53af7adb65fb1bf28e6
Emily <a2017110728@estudiantes.upsa.edu.bo>
2021-04-13 00:07:39 -0400

    Empezando proyecto, creando modelo

---
+++ practica1/work1/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="work1.WebApiApplication" Language="C#" %>
---
+++ practica1/work1/Models/Persona.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace work1.Models
+{
+    public class Persona
+    {
+        [Key]
+        public int PerosnId { get; set; }
+        [Required]
+        public string Name { get; set; }
+        public int CovidCount { get; set; }
+    }
+}
