d8280097511ac8db24f2a84151f5d77c2c1f5099
Emily <a2017110728@estudiantes.upsa.edu.bo>
2021-04-12 22:19:50 -0400

    Empezando Proyecto

---
+++ practica/Work/Models/People.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace Work.Models
+{
+    public class People
+    {
+        [Key]
+        public int PerosnId { get; set; }
+        [Required]
+        public string Name { get; set; }
+        public int CovidCount { get; set; }
+    }
+}
