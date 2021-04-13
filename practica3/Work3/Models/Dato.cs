db02c5d0fe21f0e75130e2cfb0e1db45796ce612
Emily <a2017110728@estudiantes.upsa.edu.bo>
2021-04-13 00:53:12 -0400

    Creando modelo

---
+++ practica3/Work3/Models/Dato.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace Work3.Models
+{
+    public class Dato
+    {
+        [Key]
+        public int PerosnId { get; set; }
+        [Required]
+        public string Name { get; set; }
+        public int CovidCount { get; set; }
+    }
+}
