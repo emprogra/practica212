741c94ccf8034bd99d1de796848520a4827fc5e8
Emily <a2017110728@estudiantes.upsa.edu.bo>
2021-04-13 03:23:16 -0400

    Se agrego el proyecto mvcwok

---
+++ practica3/WebWork/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(WebWork.Startup))]
+namespace WebWork
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
