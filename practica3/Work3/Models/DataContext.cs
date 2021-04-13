cfdc08767c23146b83d98aa8ff37b5ef37d26b65
Emily <a2017110728@estudiantes.upsa.edu.bo>
2021-04-13 00:58:08 -0400

    Creando DataContext

---
+++ practica3/Work3/Models/DataContext.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace Work3.Models
+{
+    public class DataContext:DbContext
+    {
+        public DataContext() : base("DefaultConnection")
+        {
+
+        }
+    }
+}
