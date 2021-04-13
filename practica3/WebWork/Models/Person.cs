287b72fb708ffe2e9878beca3b115cc57b7d95e3
Emily <a2017110728@estudiantes.upsa.edu.bo>
2021-04-13 03:41:46 -0400

    Se agrego el controlador mvc

---
+++ practica3/WebWork/Models/Person.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace WebWork.Models
+{
+    public class Person
+    {
+        [Key]
+        public int PerosnId { get; set; }
+        [Required]
+        public string Name { get; set; }
+        public int CovidCount { get; set; }
+    }
+}
